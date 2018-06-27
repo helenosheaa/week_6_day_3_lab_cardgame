d1450c102096994f73144fbe92ee407f61b1e731
leeburgess67 <leeburgessmail@gmail.com>
2018-06-27 11:56:29 +0100

    game has deck test passing

--- src/test/java/GameTest.java
+++ src/test/java/GameTest.java
@@ -1,17 +1,26 @@
+import org.junit.Before;
 import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
 
 public class GameTest {
 
+    Deck deck;
+    Player players;
+    Game game;
 
+    @Before
+    public void before(){
+        game = new Game();
+        deck = new Deck();
+        players = new Player("John");
+    }
 
     @Test
     public void gameHasDeck(){
-
+        assertEquals(52, this.deck.countDeck());
     }
 }
 
 
 
-    Game game = new Game();
-        game.shuffleDeck();
-                String string = "something";
